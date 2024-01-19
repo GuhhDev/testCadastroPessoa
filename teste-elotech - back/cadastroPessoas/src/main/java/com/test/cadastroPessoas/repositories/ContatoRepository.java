b1609b4b251594acadd452d45ffba9d1cc77e94f
Gustavo Henrique Batista <89535864+GuhhDev@users.noreply.github.com>
2024-01-19 14:54:11 -0300

    Delete teste-elotech - back/cadastroPessoas directory

--- teste-elotech - back/cadastroPessoas/src/main/java/com/test/cadastroPessoas/repositories/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.cadastroPessoas.repositories;
-
-import com.test.cadastroPessoas.entities.Contato;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContatoRepository extends CrudRepository<Contato, Long> {
-}
